1c6f06a2e3d7530ced0d3a6526444c432ab9ce28
Rob <robpolisena@gmail.com>
2020-07-02 19:41:39 +0000

    returns callback function for breed description

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,11 +1,18 @@
 const request = require('request');
   
-let input = process.argv.slice(2);
+const fetchBreedDescription = function(breedName, callback) {
 
-request('https://api.thecatapi.com/v1/breeds/search?q=' + input[0], (error, response, body) => {
-  const data = JSON.parse(body);
-  if (data[0] === undefined) {
-    console.log('you have an error', error);
-  }
-  console.log(data[0]["description"]);
-});
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, description, body) => {
+    const data = JSON.parse(body);
+    const breed = data[0];
+    if (error) {
+      return callback(error, null);
+    }  if (breed) {
+      return callback(null, breed.description);
+    } else {
+      callback('Breed not found');
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
